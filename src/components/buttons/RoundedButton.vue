6d0bf0901132f91ade1da1cb84e128f17317cae2
Riyanto1831 <stake.mural1831@gmail.com>
2021-05-10 17:15:20 +0700

    Add RoundedButton component

---
+++ src/components/buttons/RoundedButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-btn
+    :color="color"
+    :dark="dark"
+    :light="light"
+    :depressed="depressed"
+    :outlined="outlined"
+    :disabled="disabled"
+    :width="width"
+    :type="type"
+    :v-on="vOn"
+    :v-bind="vBind"
+    height="35px"
+    large
+    class="rounded-btn rounded-pill text-capitalize"
+    @click="onClick()"
+  >
+    <v-icon v-if="icon" left dark>
+      {{ icon }}
+    </v-icon>
+    {{ text }}
+  </v-btn>
+</template>
+<script>
+export default {
+  name: "RoundedButton",
+  props: {
+    text: String,
+    icon: String,
+    color: String,
+    dark: Boolean,
+    light: Boolean,
+    outlined: Boolean,
+    disabled: Boolean,
+    width: String,
+    type: String,
+    vOn: String,
+    vBind: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+<style lang="scss">
+.rounded-btn {
+  font-family: Helvetica, sans-serif;
+}
+.v-btn__content {
+  font-size: 16px;
+}
+</style>
--- src/views/Home/TestComponent/index.vue
+++ src/views/Home/TestComponent/index.vue
@@ -1,13 +1,28 @@
 <template>
+  <div>
+    <h1>Components UI Testing Page</h1>
+    <br /><br />
     <div>
-        <h1>Components UI Testing Page</h1>
+      <RoundedButton color="primary" text="Primary" icon="fa fa-pencil" dark />
     </div>
+    <div>
+      <RoundedButton color="secondary" text="Secondary" icon="fa fa-eye" dark />
+    </div>
+    <div>
+      <RoundedButton color="highlight" text="Highlight" icon="fa fa-trash" light />
+    </div>
+    <div>
+      <RoundedButton color="dark" text="Dark" dark />
+    </div>
+  </div>
 </template>
 <script>
+import RoundedButton from "@/components/buttons/RoundedButton";
 export default {
-    name: 'TestComponent',
-}
+  name: "TestComponent",
+  components: {
+    RoundedButton,
+  },
+};
 </script>
-<style lang="scss">
-    
-</style>
+<style lang="scss"></style>
